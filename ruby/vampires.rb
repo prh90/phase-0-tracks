c1df0023f0f99ce1014dc8ce9e09155cf8b164ab
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-08-23 19:35:46 -0700

    Done with release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+name = gets.chomp 
+
+puts = "How old are you?"
+age = gets.chomp.to_i
+
+puts = "What year were you born then?"
+year = gets.chomp.to_i
+
+puts = "The cafeteria is serving garlic bread, would you like some? (yes/no)"
+garlic = gets.chomp
+
+puts = "Would you like to enroll in the company’s health insurance? (yes/no)"
+insurance = gets.chomp
+
