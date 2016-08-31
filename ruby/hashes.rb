1546c133e80cf3eec800ea4e5e3bd1696b67ae38
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-08-31 01:33:48 -0700

    add pseudocode to file

---
+++ ruby/hashes.rb
@@ -0,0 +1,11 @@
+=begin
+1.Ask for clients information 
+ (Name, age, # of kids, desired theme, preferred color, matching rooms, specify if false)
+2. Save info in a variable 
+3. Use hash for Designer info
+4. Ask if mistakes or change of mind
+  a. if none then skip to end
+  b. if so correct the desired input then end
+5. Print data
+  
+=end
