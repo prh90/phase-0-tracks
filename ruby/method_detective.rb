3444ffabb47104a7f63363f16304111feb101da4
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-08-30 20:54:36 -0700

    add corresponding methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -1,39 +1,45 @@
 # Replace in the "<???>" with the appropriate method (and arguments, if any).
 # Uncomment the calls to catch these methods red-handed.
 
+
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+ "iNvEsTiGaTiOn".swapcase
+ => “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"zom".insert(1, 'o')
+ #=> “zoom”
+"zom".gsub('o','oo')
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"enhance".center(10)
+ #=> "    enhance    "
 
-# "the usual".<???>
+"Stop! You’re under arrest!".upcase
+ #=> "STOP! YOU’RE UNDER ARREST!"
+
+ "the usual".insert(9, "suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+ " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+ "The case of the disappearing last letter".chomp('r')
+# => "The case of the disappearing last lette"
+ "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+ "The mystery of the missing first letter".delete('T')
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+ "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+ "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+#Returns the codepoint of the character in the string.
 
-# "How many times does the letter 'a' appear in this string?".<???>
+ "How many times does the letter 'a' appear in this string?".count('a')
 # => 4
