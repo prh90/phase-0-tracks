f0053983ea4209b1e468a8e9995f0c5402f05f20
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-08-31 02:27:25 -0700

    Update with final code

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -34,11 +34,24 @@ elsif matching == "no".to_s
 end
 
 designer_info = {
-  "Name" => name,
-  "Age" => age,
-  "Kids" => kids,
-  "Theme" => theme,
-  "Matching" => matching,
-  "Specify" => specify
+  :name => name,
+  :age => age,
+  :kids => kids,
+  :theme => theme,
+  :matching => matching,
+  :other => specify
 }
+p designer_info
+
+puts "\n\n\nCaught any mistakes? You can fix them by simply typing the desired area that needs to be changed (:name, :age, :kids, :theme, :matching) if no changes need to be made please enter 'none'"
+fix = gets.chomp.to_s
+
+if fix == "none"
+  puts "Information was saved, Thank you!"
+else
+  puts "Enter new data #{fix}"
+  update = gets.chomp
+  designer_info[fix.to_sym] = update
+end
+
 p designer_info
