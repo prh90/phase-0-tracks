4ba9b341e1bac3869205d83d3f19d6872d787e7a
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-07 23:24:08 -0700

    Finished 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,25 @@
+=begin
+
+
+SPECIES: 
+-Canine
+_____________________________
+CHARACTERISTICS 
+
+Metabolism: High
+Friendly: yes
+Eye count: 2
+Leg count: 4
+weight: varies
+Height: varies
+Color: varies
+Hair length: varies
+_____________________________
+
+BEHAVIOR: 
+-Drooling
+-Barking
+-Digging
+-Tail Chaser
+  
+=end
