63ee8e5cb7cf5a41346e8e555c20177fb5bb6ceb
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-08 11:45:49 -0700

    Add basic pup class code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,26 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  def speak(int)
+    int.times do
+      p "Woof!"
+    end
+  end
+  def roll_over
+    puts "*Rolls over*"
+  end
+  def dog_years(human_years)
+    human_years * 7
+  end
+  def sit
+  puts "sits down"
+  end
 
 end
 
+pup=Puppy.new()
+pup.fetch("ball")
+pup.speak(2)
+pup.roll_over
+pup.dog_years(4)
+pup.sit
