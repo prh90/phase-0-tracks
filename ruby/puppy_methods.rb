09d8e328c34dec66be6b6e3f192dc3dc28f37765
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-08 12:53:06 -0700

    Creat a programmer class with instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -27,3 +27,42 @@ pup.speak(2)
 pup.roll_over
 pup.dog_years(4)
 pup.sit
+
+
+class Programmer
+  def initialize
+    puts "Initializing new programmer instance ... "
+    @name = ["Pablo", "Dawit", "Johnnie", "Obama", "Osama"]
+    @language = ["Ruby", "Jdango", "C++", "Java"]
+    @beverage = ["Coffe", "Beer", "Energy drink", "Bourbon"]
+    @status = ["stressing!", "Happy", "Confused", "Done", "EMO"]
+    
+  end
+  def drink(int)
+    puts "Drinks #{int} Cups of #{@beverage.sample}"
+  end
+  def code
+    puts "Coding some #{@language.sample}"
+  end
+  def status
+    puts "#{@name.sample} is currently #{@status.sample}"
+    
+  end
+end
+
+programmers = []
+15.times do 
+  programmers << Programmer.new()
+end
+programmers.each do |programmer|
+  puts "________________________________"
+  puts programmer.status
+  puts programmer.drink(rand(7))
+  puts programmer.code
+  
+end
+
+
+
+
+
