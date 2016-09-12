2b9bcbc15982dcdf10ac69c763b0679d75a710e0
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-11 18:53:50 -0700

    Add empty mathods to game class

---
+++ ruby/guessing_game/guessing_game.rb
@@ -0,0 +1,26 @@
+class GuessingGame
+
+  def initialize
+
+  end
+
+  def word_check(guess)
+    # Checks guess with word
+    # If user guesses correctly then the letter will be revealed.
+  end
+
+  def user_switch(guess)
+    # If user guesses correct continue
+    # If user guesses wrong switch to other user
+  end
+
+  def guess_limit(guess)
+    # sets limit to word.
+  end
+
+  def final_result(guess)
+    # Tracks if user won or lost and displays proper message
+  end
+
+end
+
