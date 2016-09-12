82e15da0ab7b103e02c371ac5b7f839f31880281
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-12 03:28:06 -0700

    add loop and final code

--- ruby/guessing_game/guessing_game.rb
+++ ruby/guessing_game/guessing_game.rb
@@ -1,44 +1,96 @@
 class GuessingGame
-  attr_reader :word, :guess_count, :game_over, :user_guesses, :guess_array, :guess_limit
-  attr_reader :guess_count
-
-  def initialize(word)
-    @word = word #accessible throughout
-    @game_over = false #does not end game until its true
-    @guess_limit = word.length #limits the guessing to length of word
-    @guess_count = 0 #starts counter at 0
-    @user_guesses = [] #To save what the user entered
-    @under_array = []
-    @word.times(@under_array << "_") #change the word with underscore
-    @won = false
-    @lost = false
-
-  end
-
-  def guess_store(guess)
-    # Receives guess and stores it into array
-    @guess_count += 1
-    @user_guesses << guess.downcase
-
-  end
-
-
-  end
-
-  def ret_message
-    if @won == true
-    puts "You won! you guessed the word #{@word} in #{@guess_count} guesses"
+  attr_reader :word,  :new_arr, :guesses, :under_arr,  :guess_limit, :guess_count
+    attr_accessor :guess_count
+    
+    def initialize(wstr)
+      @word = wstr.downcase
+      @new_arr = wstr.downcase.split('')
+      @guess_limit = wstr.length 
+      @guesses = []
+      @guess_count = 0 
+      @under_arr = []
+      @word.length.times{@under_arr<< "_" }
+      @win = false 
+      @lose = false
     end
-    if @lost == true
-    puts "You lost! it was so easy, it was #{@word}"
+    
+    def guess_input(guess)
+      @guess_count += 1 
+      @guesses << guess.capitalize
+      puts "This is guess #{@guess_count} of #{@guess_limit}."
     end
-  end
+    
+    def comparing_str(guess)
+      @guesses.include? guess
+      # if @guesses.include? guess
+      #   puts  "That's a repeat guess. Try again"
+      # end
+    end
+    
+    def string(guess)
+    @word.include? guess
+    end
+    
+    def check(guess, guesses, under_Arr)
+      guess_arr = guess.downcase.split("")
+      for i in 0..guess_arr.length-1
+        for j in 0..guesses.length-1
+            if guesses[j] == guess_arr[i]
+              under_arr[j] = guess_arr[i]
+            end
+          end    
+        end
+      @under_arr =  under_Arr 
+    end
+    
+    def under_Arr(under_arr)
+      under_arr.each{|x| print x,' '}
+      puts ""
+    end 
+    
+    def end_game(new_arr, under_Arr)
+      if under_Arr == new_arr
+        @win = true
+      elsif  @guess_count > @guess_limit
+        @lose = true 
+      end
+      @win or @lose
+    end 
+    
+    def final_message
+      if @win == true
+      puts "You won and it took you #{@guess_count} guesses. You found the word #{@word.capitalize}."
+      elsif @lose == true
+      puts "You reached the max amount of guesses, #{@guess_limit}, Unfortunately you didnt guess the word which was #{@word.capitalize}" 
+      end
+    end
+end 
+puts "Welcome to the word guessing game!!"
+puts "\nPlayer 1 please enter the word that player2 needs to guess"
+user_word = gets.chomp 
 
+game = GuessingGame.new(user_word)
+
+loop do
+  puts "Player2 put on your thinking cap!"  
+  puts "Enter your guess:"
+  guess =gets.chomp
+  if game.comparing_str(guess)
+    puts "That's a repeat guess. Try again"
+  elsif game.string(guess)
+    game.check(guess, game.new_arr, game.under_arr)
+    puts "Correct"
+    game.guess_input(guess)
+  else
+    puts "Incorrect"
+    game.guess_input(guess)
+  end
+  game.under_Arr(game.under_arr)
+  break if game.end_game(game.new_arr, game.under_arr)
 end
+game.final_message
 
-puts "Welcome to the Word Guessing Game!"
-puts "Please enter a word to start the game"
-word = gets.chomp.split('')
 
-game = GuessingGame.new(word)
+
+
 
