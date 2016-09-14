7ad3163182ea1973bae6b6fc501afd7d3ebefb77
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-14 02:52:54 -0700

    add compare function

--- js/algos.js
+++ js/algos.js
@@ -9,3 +9,14 @@ function find(arr){
   console.log(arr.sort(function(a, b) { return b.length - a.length})[0]);
 }
 find(["long phrase","longest phrase","longer phrase"]);
+
+//Take two objects
+//set the match to false until proven true later
+//have both objects compare keys, if a match is found 
+//reassign match with true
+
+function compare(obj1, obj2){
+  match = false
+  
+  return match
+}
