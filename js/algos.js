565718a67e4b4032f03da23288dceba7a9264d24
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-14 22:21:26 -0700

    finished compare of objects

--- js/algos.js
+++ js/algos.js
@@ -5,18 +5,28 @@
 //The string of greater length would be moved to index 0
 //print out index 0
 
-function find(arr){
-  console.log(arr.sort(function(a, b) { return b.length - a.length})[0]);
-}
-find(["long phrase","longest phrase","longer phrase"]);
+// function find(arr){
+//   console.log(arr.sort(function(a, b) { return b.length - a.length})[0]);
+// }
+// find(["long phrase","longest phrase","longer phrase"]);
 
 //Take two objects
 //set the match to false until proven true later
 //have both objects compare keys, if a match is found 
 //reassign match with true
 
-function compare(obj1, obj2){
-  match = false
-  
-  return match
-}
+function compare(object1, object2) {
+  var match = false;
+  for (var obj1Key in object1) {
+     if (object2.hasOwnProperty(obj1Key)) {
+      if (object1[obj1Key] === object2[obj1Key]) {
+         match = true;
+        }
+      }
+    }
+    console.log(match);
+  }
+ compare({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+ 
+
+
