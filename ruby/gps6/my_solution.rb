3bfb3cfcb9c21cf57fc557c1ac53b9647a36d9ee
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-16 19:33:15 -0700

    Complete gps 3.1

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,47 +1,53 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: Guide ].
+# We spent [1.5] hours on this challenge.
 
 # EXPLANATION OF require_relative
 #
 #
 require_relative 'state_data'
+# Both files are in the Same directory in this case. 
+# Imports the data from the other file to run.
 
 class VirusPredictor
-
+# Only accessible inside class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      fraction =  0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      fraction = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      fraction = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      fraction = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      fraction = 0.05
     end
+
+    number_of_deaths = (@population * fraction).floor
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread
+    #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -70,18 +76,46 @@ end
  # initialize VirusPredictor for each state
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
+
+STATE_DATA.each do |key, value|
+  state = VirusPredictor.new(key, value[:population_density], value[:population])
+  state.virus_effects
+end
 
 
 #=======================================================================
 # Reflection Section
+=begin
+What are the differences between the two different hash syntaxes shown in the state_data file?
+String and symbol
+
+What does require_relative do? How is it different from require?
+require relative is relative to the code you already have.
+require itself is loading a file that if specifically tailored for your code
+
+What are some ways to iterate through a hash?
+.each method
+or a .sort method
+
+When refactoring virus_effects, what stood out to you about the variables, if anything?
+That they did not have to be present. It was excess code.
+
+What concept did you most solidify in this challenge?
+The question is what didn't receive help on that helped me better understand.
+More about accessor/ reader, refactor, instance variable, global, class, require_relative.
+This whole exercise was a boost to my learning.
+  
+
+  
+=end
