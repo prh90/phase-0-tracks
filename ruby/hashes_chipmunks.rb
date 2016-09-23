7ab779488a8d1ae10cbea5d865d5263840c5a435
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-23 13:53:49 -0700

    Create new hash work included psuedo

---
+++ ruby/hashes_chipmunks.rb
@@ -0,0 +1,15 @@
+=begin
+1. Ask for the clients information
+  -Name
+  -Age 
+  -# kids
+  -Matching rooms?
+  -Specify if user does not want matching rooms
+2. Save new information into variables
+3. Use logic to ask user if matching room or not else ask for new info
+4. Have hash store information(designer info)
+5. Ask if mistakes or change of mind
+  a. if none then skip to end
+  b. if so correct the desired input then end
+6. Print data 
+=end
