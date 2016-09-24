cc6c9b7b7d03c41209c54a87e8189016f258c1b4
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-24 00:28:59 -0700

    Add iteration and map! to code

--- ruby/iteration_chipmunks.rb
+++ ruby/iteration_chipmunks.rb
@@ -4,3 +4,12 @@ def method_block
   puts "---inside method after block----"
 end
 method_block {|name1, name2| puts "This is #{name1} and his middle name is #{name2}"}
+
+cool_array = ["Batman", "Iron-man", "Deadpool", "Lazy-man"]
+cool_array.each {|x| puts x}
+
+puts #Put this here for spacing on gitbash
+
+cool_array.map! do |x|
+  puts x.upcase
+end
