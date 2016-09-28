3b3f2294390f6d68f2822c861ee2d8d1b8a0e1fe
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-27 22:26:35 -0700

    Add driver code to class

--- ruby/santa_chipmunks.rb
+++ ruby/santa_chipmunks.rb
@@ -1,15 +1,32 @@
-def initialize
-  puts "Initializing Santa instance ..."
+class Santa
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @age = 0
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  end
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(type)
+    puts "That was a good #{type}"
+  end
+
+# Driver code________________
+# initialize()
+# speak()
+# eat_milk_and_cookies("Peanut butter cookie")
+# ____________________________________________
+end
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
 
-def speak
-  puts "Ho, ho, ho! Haaaappy holidays!"
-end
+p santas
 
-def eat_milk_and_cookies(type)
-  puts "That was a good #{type}"
-end
-
-initialize()
-speak()
-eat_milk_and_cookies("Peanut butter cookie")
