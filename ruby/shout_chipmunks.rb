b08869fe660c944d183ef698327d9d14d67223cc
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-27 23:38:02 -0700

    Add driver code

--- ruby/shout_chipmunks.rb
+++ ruby/shout_chipmunks.rb
@@ -1,3 +1,12 @@
 module Shout
-  
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    words + "!!!" + " :("
+  end
 end
+
+puts Shout.yell_angrily("This hw is too long")
+puts Shout.yelling_happily("Im almost done")
