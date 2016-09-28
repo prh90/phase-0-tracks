842e3364af5d603acd41f9bcb888f4b99de7ef9f
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-09-27 21:49:25 -0700

    Add eat milk and cookies method with driver code to test

--- ruby/santa_chipmunks.rb
+++ ruby/santa_chipmunks.rb
@@ -1,3 +1,11 @@
 def speak
   puts "Ho, ho, ho! Haaaappy holidays!"
 end
+
+def eat_milk_and_cookies(type)
+  puts "That was a good #{type}"
+end
+
+speak()
+
+eat_milk_and_cookies("Peanut butter cookie")
