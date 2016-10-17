e7d87987f5835514eaf9221ee6dee40453575756
Pablo R. Hernandez <pablorhernandez1@gmail.com>
2016-10-17 02:02:53 -0700

    Add database also add code to rb file to start project

--- databases/food_intake/food_counter.rb
+++ databases/food_intake/food_counter.rb
@@ -1,13 +1,24 @@
 require 'sqlite3'
+require 'date'
 
 # Creating a database
-db = Sqlite3::Database.new("foodlist.db")
+db = SQLite3::Database.new("foodlist.db")
 db.results_as_hash = true
 
+
+# Create a table for information
 create_table_cmd = <<-SQL 
   CREATE TABLE IF NOT EXISTS foodlist(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
+    date TEXT,
+    food VARCHAR(255),
     calories INT
   )
 SQL
+
+db.execute(create_table_cmd)
+
+def add_food(food)
+  db.execute("INSERT INTO foodlist (food, calories) VALUES (? , ?)",[food])
+end
+
